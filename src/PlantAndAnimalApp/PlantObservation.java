e9e318948091dfe9902a56b52a6de9b17c7675d2
safiatdao <107083240+safiatdao@users.noreply.github.com>
2026-03-18 23:53:11 +0000

    Update PlantObservation.java

--- src/PlantAndAnimalApp/PlantObservation.java
+++ src/PlantAndAnimalApp/PlantObservation.java
@@ -18,16 +18,17 @@ public class PlantObservation extends Observation {
         this.TypeOfPlant = TypeOfPlant;
     }
     
-    
+    //this update method changes the plant type
     public void update(String additionalDetail){
         this.TypeOfPlant = additionalDetail;
     }
     
-    
+    //This updates all fields
 public void update(String personsName, String location, String date, String TypeOfPlant) {
     super.update(id,personsName, location, date, TypeOfPlant);
     this.TypeOfPlant = TypeOfPlant;
 }
+//override the displayDetail() to show details of plant observations
     @Override
      public String displayDetail(){
         return "Plant Id-  "+ id + "\n" + "Name-  "+ personsName + "\n" + "Location-  " + location + "\n" + "Date-  " + date + "\n" + "Plant-  " + TypeOfPlant; 
